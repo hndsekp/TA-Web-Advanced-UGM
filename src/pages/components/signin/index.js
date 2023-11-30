7ff64bf2f59063da281ea1a0f1abf509737da817
unknown <alhendesekaaa08@gmail.com>
2023-11-30 14:56:30 +0700

    added sign up component

--- src/pages/components/signin/index.js
+++ src/pages/components/signin/index.js
@@ -10,9 +10,9 @@ import {
   Group,
   Button,
 } from "@mantine/core";
-import classes from "./auth.module.css";
+import classes from "./signup.module.css";
 
-export function Auth() {
+export function Signup() {
   return (
     <Container size={420} my={40}>
       <Title ta="center" className={classes.title}>
@@ -20,29 +20,24 @@ export function Auth() {
       </Title>
 
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email / Username" placeholder="Your Email / Username" required />
+        <TextInput label="Username" placeholder="Your Username" required/>
+        <TextInput label="Email" placeholder="Your Email" required />
         <PasswordInput
+          
           label="Password"
           placeholder="Your password"
           required
-          mt="md"
+          
         />
-        <Group justify="space-between" mt="lg">
-          <Checkbox label="Remember me" />
-          <Anchor component="button" size="sm">
-            Forgot password?
-          </Anchor>
-        </Group>
         <Button className={classes.buttonsi} fullWidth mt="xl">
-          Sign in
+          Sign Up
         </Button>
         <Text c="dimmed" size="sm" ta="center" mt={5}>
-          Don't have an account?{" "}
-          <Anchor className={classes.createsi} size="sm" ta="center" component="button">
-            Create account
+          Already have an account?{" "}
+          <Anchor  size="sm" ta="center" component="button">
+            Sign In 
           </Anchor>
         </Text>
-        
       </Paper>
     </Container>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import { Auth } from "./components/authentication";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
 import { Hero } from "./components/hero";
+import { Navbar } from "./components/navbar";
+import { Signup, Singin } from "./components/signin";
 
 export default function Home() {
   return (
