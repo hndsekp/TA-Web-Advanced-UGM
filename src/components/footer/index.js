60cc3843080d0d2e48972e6b55daebf6b880a8d0
Aytsrp <ranukekeaja@gmail.com>
2023-12-08 10:45:45 +0700

    trying to redeploy 4

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,12 +1,10 @@
 import { Container, Image } from '@mantine/core';
-import { MantineLogo } from "../../../public/assets/logo3.png";
 import classes from './footer.module.css';
 
 export function Footer() {
   return (
     <div className={classes.footer}>
       <Container className={classes.inner}>
-      <Image className={classes.fotoFooter} src={MantineLogo}/>
         <p className={classes.footerBot}>Copyright &copy;2023; Designed by <span class="designer">Alhendes & Ranu</span></p>
       </Container>
     </div>
