8c645b620f270a27ba24674e857d6927a4270f17
Aytsrp <ranukekeaja@gmail.com>
2023-12-08 10:24:26 +0700

    trying to redeploy 3

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
   user: 'postgres',
   host: 'localhost',
   database: 'db_lolycones',
