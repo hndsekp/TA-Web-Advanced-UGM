25c0850e0c7ab738670600b50640d4ac7b501ea6
unknown <alhendesekaaa08@gmail.com>
2023-12-10 15:34:20 +0700

    testing responsive and change favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
 import { Hero } from "@/components/hero";
+import { Slider } from "@/components/carousel";
+import Rasa from "@/components/grid";
+import { ProductPage } from "@/components/product";
+import { BuyPage } from "@/components/buy";
 
 export default function Home() {
   return (
       <>
       <Navbar/>
       <Hero/>
+      <Slider/>
+      <Rasa/>
+      <ProductPage/>
+      <BuyPage/>
       <Footer/>
       </>
   )
