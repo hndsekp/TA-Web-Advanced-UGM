08b2959d0fa0e9d0d6916102639cfcf4452d26c7
Aytsrp <ranukekeaja@gmail.com>
2023-12-11 03:10:16 +0700

    CRUD success

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,27 +4,27 @@ import Logo from "/public/assets/logo3.png";
 import classes from "./navbar.module.css";
 import { IconShoppingCart } from "@tabler/icons-react";
 import { useRouter } from "next/router";
+import Link from "next/link";
+
+const data = [
+  {link: "/", label:"Home"},
+  {link: "/product", label:"Products"},
+  {link: "/", label:"Flavors"},
+  {link: "/", label:"About Us"},
+]
 
 export function Navbar() {
   const router = useRouter();
+  const link = data.map((item) => (
+    <Link className={classes.link}
+    href={item.link}>{item.label}</Link>))
   return (
     <Box>
       <header className={classes.header}>
         <Group justify="space-between" h="100%">
           <Image src={Logo} width={120} alt="Lolycones"/>
           <Group h="100%" gap={0} visibleFrom="sm">
-            <a href="/" className={classes.link}>
-              Home
-            </a>
-            <a href="/product" className={classes.link}>
-              Products
-            </a>
-            <a href="#" className={classes.link}>
-              Flavors
-            </a>
-            <a href="#" className={classes.link}>
-              About Us
-            </a>
+            {link}
           </Group>
 
           <Group visibleFrom="sm">
